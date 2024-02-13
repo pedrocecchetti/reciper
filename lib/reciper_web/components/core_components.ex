13ef4a17418497c3f33d2222ca71aa75f37496b9
PedroCa <pedro.cecchetti@gmail.com>
2024-02-13 14:57:48 +0100

    feat: update show.html and style

--- lib/reciper_web/components/core_components.ex
+++ lib/reciper_web/components/core_components.ex
@@ -535,10 +535,10 @@ defmodule ReciperWeb.CoreComponents do
 
   def list(assigns) do
     ~H"""
-    <div class="mt-14 w-6/12">
+    <div class="mt-14">
       <dl class="my-4 divide-y divide-zinc-100">
-        <div :for={item <- @item} class="flex gap-4 py-4 text-sm leading-6 sm:gap-8">
-          <dt class="w-1/4 flex-none text-zinc-500"><%= item.title %></dt>
+        <div :for={item <- @item} class="flex gap-4 py-4 text-sm sm:gap-8">
+          <dt class="w-6/12 text-zinc-500 font-black"><%= item.title %></dt>
           <dd class="text-zinc-700"><%= render_slot(item) %></dd>
         </div>
       </dl>
