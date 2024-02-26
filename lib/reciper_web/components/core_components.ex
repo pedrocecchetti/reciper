91c052c6b8cdeab2fca1aa7591c13a01e527ca73
PedroCa <pedro.cecchetti@gmail.com>
2024-02-26 17:32:45 +0100

    feat: adds new flash type (:success)

--- lib/reciper_web/components/core_components.ex
+++ lib/reciper_web/components/core_components.ex
@@ -100,7 +100,7 @@ defmodule ReciperWeb.CoreComponents do
   attr :id, :string, doc: "the optional id of flash container"
   attr :flash, :map, default: %{}, doc: "the map of flash messages to display"
   attr :title, :string, default: nil
-  attr :kind, :atom, values: [:info, :error], doc: "used for styling and flash lookup"
+  attr :kind, :atom, values: [:info, :error, :success], doc: "used for styling and flash lookup"
   attr :rest, :global, doc: "the arbitrary HTML attributes to add to the flash container"
 
   slot :inner_block, doc: "the optional inner block that renders the flash message"
@@ -116,8 +116,9 @@ defmodule ReciperWeb.CoreComponents do
       role="alert"
       class={[
         "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
-        @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
-        @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
+        @kind == :success && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
+        @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900",
+        @kind == :info && "bg-blue-50 text-blue-900 shadow-md ring-blue-500 fill-blue-900"
       ]}
       {@rest}
     >
@@ -147,7 +148,8 @@ defmodule ReciperWeb.CoreComponents do
   def flash_group(assigns) do
     ~H"""
     <div id={@id}>
-      <.flash kind={:info} title="Success!" flash={@flash} />
+      <.flash kind={:info} title="INFO!" flash={@flash} />
+      <.flash kind={:success} title="Success!" flash={@flash} />
       <.flash kind={:error} title="Error!" flash={@flash} />
       <.flash
         id="client-error"
